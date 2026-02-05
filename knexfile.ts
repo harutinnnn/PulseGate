53ead2eba269f4ecbea1682eb36388c64f319380
harut <harutinnnn@gmail.com>
2026-02-05 12:11:31 +0400

    Commit

---
+++ knexfile.ts
@@ -0,0 +1,18 @@
+import type { Knex } from "knex";
+import path from "path";
+
+const config: { [key: string]: Knex.Config } = {
+    development: {
+        client: "sqlite3",
+        connection: {
+            filename: path.join(__dirname, "data", "database.db")
+        },
+        useNullAsDefault: true,
+        migrations: {
+            extension: "ts",
+            directory: "./migrations"
+        }
+    }
+};
+
+export default config;
