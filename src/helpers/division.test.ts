774fbf6a27e77068e7de15ba7f8ac62a9b3fae53
harut <harutinnnn@gmail.com>
2026-02-21 22:08:31 +0400

    Push actions

--- src/helpers/division.test.ts
+++ src/helpers/division.test.ts
@@ -4,4 +4,8 @@ describe('mathematics', () => {
   it('should return the number of divisions', async () => {
     expect(division(4, 2)).toBe(2);
   });
+
+  test('should return Infinity', async () => {
+    expect(division(4, 0)).toBe(Infinity);
+  });
 });
